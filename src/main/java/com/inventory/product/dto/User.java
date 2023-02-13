0853acaf1ec03db62b458a831ca72292ec22bc44
Sheel Bouddh <bouddhsheel@gmail.com>
2023-02-13 16:49:19 +0530

    rest added

---
+++ src/main/java/com/inventory/product/dto/User.java
@@ -0,0 +1,16 @@
+package com.inventory.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private int userId;
+    private String userRole;
+}
--- src/main/java/com/inventory/product/service/InventoryService.java
+++ src/main/java/com/inventory/product/service/InventoryService.java
@@ -2,12 +2,18 @@ package com.inventory.product.service;
 
 import com.inventory.product.dto.Product;
 import com.inventory.product.dao.ProductRepositoryImpl;
+import com.inventory.product.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
 public class InventoryService {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     private ProductRepositoryImpl productRepository;
 
@@ -24,7 +30,12 @@ public class InventoryService {
     }
 
     public void addProduct(Product product) {
-        int userId = product.getSellerId();
+//        int userId = product.getSellerId();
+//        String url = "xyz";
+//        String userRole = restTemplate.getForObject(url, User.class).getUserRole();
+//        if(userRole == "Seller"){
+//            productRepository.create(product);
+//        }
         productRepository.create(product);
     }
 
