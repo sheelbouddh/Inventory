26db2c6cf0593acdf01d498e4fa5487533b327c1
Sheel Bouddh <bouddhsheel@gmail.com>
2023-02-16 12:04:30 +0530

    added rest api call to acl app

---
+++ src/main/java/com/inventory/product/dto/User.java
@@ -0,0 +1,15 @@
+package com.ACL_App.ACL.APP;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class User{
+
+    private int User_ID;
+    private String User_Name;
+    private int Role_ID;
+}
